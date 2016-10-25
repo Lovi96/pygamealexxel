61c6ae7bdf6eac9dc7b6d413b830fb828309331d
bodoralex <bodaleaa@gmail.com>
2016-10-25 14:14:07 +0200

    nemműköd

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+def nyomtat():
+    for i in range(0, 10):#                            a kiíratás 
+        for o in range(0, 10):
+            if aArray[i][o] == 0:
+                print("[ ]",end="")
+            if aArray[i][o] == 1:
+                print("[X]",end="")
+            if aArray[i][o] == 2:
+                print("[O]",end="")
+            if aArray[i][o] == 3:
+                print("[#]",end="")
+            if o == 9:
+                print("\n")
+
+
 aArray = []
 bArray = []
 
 fivelengthship = [3,3,3,3,3]
+fourlengthship = [3,3,3,3]
+threelengthship = [3,3,3]
+twolengthship = [3,3]
 
+ships = [twolengthship, threelengthship, fourlengthship, fivelengthship]
 
 new = []
 for i in range (0, 10):#                         tömb inicializálása
@@ -16,32 +35,33 @@ for i in range (0, 10):
     bArray.append(new)
     new = []
 
-aArray[2][2] = 3
-bArray[3][3] = 3
 
 positionx = int(input("adjad meg az x pozitront!"))
 positiony = int(input("adjad meg az y pozitront!"))
 ansver = input("föggőleges legyene?  y/n")
-if ansver == "n":
-    for q in range(len(fivelengthship)):
-        aArray[positiony-1][q+positionx-1] = fivelengthship[q]
-if ansver == "y":
-    for q in range(len(fivelengthship)):
-        aArray[q+positiony-1][positionx-1] = fivelengthship[q]
 
 
-for i in range(0, 10):#                            a kiíratás
-    for o in range(0, 10):
-        if aArray[i][o] == 0:
-            print("[ ]",end="")
-        if aArray[i][o] == 1:
-            print("[X]",end="")
-        if aArray[i][o] == 2:
-            print("[O]",end="")
-        if aArray[i][o] == 3:
-            print("[#]",end="")
-        if o == 9:
-            print("\n")
+
+for w in ships:
+    index = 2
+    positionx = int(input("adjad meg a"+w+" x pozitronÁT!"))
+    positiony = int(input("adjad meg az"+w+" y pozitront!"))
+    ansver = input("föggőleges legyene?  y/n \n")
+
+    if ansver == "n":
+        for index in range(len(fivelengthship)):
+            aArray[positiony-1][index+positionx-1] = fivelengthship[q]
+    if ansver == "y":
+        for index in range(len(fivelengthship)):
+            aArray[index+positiony-1][positionx-1] = fivelengthship[q]
+    nyomtat()
+    index += 1
+
+
+
+
+
+
 
 
 
