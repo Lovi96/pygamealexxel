ab70e1c47fca508b3b8de3d58e03cac789eb7f8d
bodoralex <bodaleaa@gmail.com>
2016-10-25 15:13:38 +0200

    blank line is life

--- main.py
+++ main.py
@@ -35,26 +35,6 @@ for i in range (0, 10):
     new = []
 
 
-
-
-
-'''for w in ships:
-    index = 2
-    positionx = int(input("adjad meg a"+w+" x pozitronÁT!"))
-    positiony = int(input("adjad meg az"+w+" y pozitront!"))
-    ansver = input("föggőleges legyene?  y/n \n")
-
-    if ansver == "n":
-        for index in range(len(fivelengthship)):
-            aArray[positiony-1][index+positionx-1] = fivelengthship[q]
-    if ansver == "y":
-        for index in range(len(fivelengthship)):
-            aArray[index+positiony-1][positionx-1] = fivelengthship[q]
-    nyomtat()
-    index += 1'''
-
-
-
 for q in range(2, 6):
     positionx = int(input("adjad meg a x {} pozitronÁT!".format(q)))
     positiony = int(input("adjad meg a y {} pozitronÁT!".format(q)))
@@ -68,22 +48,3 @@ for q in range(2, 6):
         for index in range(q):
             aArray[index+positiony-1][positionx-1] = fivelengthship[q-2]
     nyomtat()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
