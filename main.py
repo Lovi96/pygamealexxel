23cdb5cb2ee54efec9bf1e3ed4d25c1e1b94ca53
bodoralex <bodaleaa@gmail.com>
2016-10-25 12:15:47 +0200

    megy a print

--- main.py
+++ main.py
@@ -12,7 +12,14 @@ for i in range (0, 10):
     bArray.append(new)
     new = []
 
+aArray[2][2] = 1
+bArray[3][3] = 1
 
-
-aArray[6][4] = 1
-print(aArray)
+for i in range(0, 10):
+    for o in range(0, 10):
+        if aArray[i][o] == 0:
+            print("[ ]",end="")
+        if aArray[i][o] == 1:
+            print("[x]",end="")
+        if o == 9:
+            print("\n")
