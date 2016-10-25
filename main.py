baad479cb3efa67a41b14571e2916b368bc5fba1
bodoralex <bodaleaa@gmail.com>
2016-10-25 11:51:37 +0200

    first lol

---
+++ main.py
@@ -0,0 +1,16 @@
+aArray = []
+bArray = []
+new = []
+for i in range (0, 10):
+    for j in range (0, 10):
+        new.append(0)
+        for j in range (0, 10):
+        new.append(0)
+    aArray.append(new)
+    new = []
+
+
+
+
+aArray[6][4] = 1
+print(aArray)
