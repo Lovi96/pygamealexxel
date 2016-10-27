a1a26ccb03aca3689677876bcffc02f0e856b33b
bodoralex <bodaleaa@gmail.com>
2016-10-27 23:38:22 +0200

    switch players + format

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+import os
+import time
+
 def nyomtat(whichPlayer):
     for i in range(0, 10):
-        if i > 0:
+        if i == 9:
+            print(i+1," ",end="")
+        elif i > 0:
             print(i+1,"  ",end="")
         for o in range(0, 10):
-            if i == 0 and o == 0:###############################
+            if i == 0 and o == 0:
                 print("     ",end ="")
-                for p in range(0, 10):#########             #ABC
+                for p in range(0, 10):
                     print(abc[p]," ",end="")
                 print("\n")
-                print("1   ",end="")#####################################  
+                print("1   ",end="")
             if whichPlayer == "a":
                 if aArray[i][o] == 0:
                     print("[ ]",end="")
@@ -44,6 +49,7 @@ def nyomtat(whichPlayer):
                     print("[B]",end="")
                 if o == 9:
                     print("\n")
+
             if whichPlayer == "aOcean":
                 if bArray[i][o] == 0:
                     print("[ ]",end="")
@@ -61,6 +67,7 @@ def nyomtat(whichPlayer):
                     print("[ ]",end="")
                 if o == 9:
                     print("\n")
+
             if whichPlayer == "bOcean":
                 if aArray[i][o] == 0:
                     print("[ ]",end="")
@@ -82,6 +89,7 @@ def textToInt():
     valueerror = 1    
     while valueerror == 1:       
         csoda = input("Give the X coordinate! (A-J) ")
+        csoda = csoda.capitalize()
         hilfe = 0
         for s in range(0, 10):
             if abc[s] == csoda:
@@ -90,9 +98,16 @@ def textToInt():
         if hilfe == 1:
             valueerror=0
             return csoda
-            
 
+def playerSwitch():
+    print("Switching players!")
+    for i in range(5,0,-1):
+        print(i)
+        time.sleep(1)
+    print('\n' * 50)
+    os.system('cls' if os.name=='nt' else 'clear')
 
+           
 aArray = []
 bArray = []
 fivelengthship = [6,6,6,6,6]
@@ -116,7 +131,7 @@ shipsNames = ["Submarine", "Cruiser", "Mothership", "Battleship"]
 abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 
 new = []
-for i in range (0, 10):#                                             tömb inicializálása
+for i in range (0, 10):#                                             tömbök inicializálása
     for j in range (0, 10):
         new.append(0)
     aArray.append(new)
@@ -128,36 +143,36 @@ for i in range (0, 10):
     new = []
 
 nyomtat("a")
-for q in range(2, 6):#                                                  a jatekos hajoi
-    
+for q in range(2, 6):#                                                  A jatekos hajoi
     print("Give the positions of the {}!  ".format(shipsNames[q-2]))
     positionx = textToInt()
     positiony = int(input("Give the Y position of the {}! (1-10) ".format(shipsNames[q-2])))
     answer = input("Do you want the {} vertically? (Y/N) ".format(shipsNames[q-2]))
+    answer = answer.capitalize()
  
-  
     for index in range(q):
         if answer == "N":
             aArray[positiony-1][index+positionx] = ships[q-2][index]
         if answer == "Y":
             aArray[index+positiony-1][positionx] = ships[q-2][index]
-   
     nyomtat("a")
-for q in range(2, 6):#                                                  a jatekos hajoi
+playerSwitch()
+nyomtat("b")
+for q in range(2, 6):#                                                  B jatekos hajoi
     print("Give the positions of the {}!  ".format(shipsNames[q-2]))
     positionx = textToInt()
     positiony = int(input("Give the Y position of the {}! (1-10) ".format(shipsNames[q-2])))
     answer = input("Do you want the {} vertically? (Y/N) ".format(shipsNames[q-2]))
-  
+    answer = answer.capitalize()
   
     for index in range(q):
         if answer == "N":
             bArray[positiony-1][index+positionx] = ships[q-2][index]
         if answer == "Y":
             bArray[index+positiony-1][positionx] = ships[q-2][index]
-   
     nyomtat("b")
-while True:
+playerSwitch()
+while True:#                                                             PEW PEW
     if (aSubmarine == 0) and (aBattleship == 0) and (aMothership == 0) and (aCruiser == 0):
         print("\n\nPLAYER 2 WINS!!!"*3)
         break
@@ -165,6 +180,7 @@ while True:
         print("\n\nPLAYER 1 WINS!!!"*3)
         break
     nyomtat("a")
+    print("      ↑↑↑↑↑↑↑ Your table ↑↑↑↑↑↑↑\n      ↓↓↓↓↓↓↓ Enemy table↓↓↓↓↓↓↓")
     nyomtat("aOcean")
     print("\n\nPlayer 1: Where would you like to shoot?")
     positionx = textToInt()
@@ -181,7 +197,7 @@ while True:
         if (bSubmarine < pbSubmarine) and (bSubmarine == 0):
             print("Player 2's Submarine sank. ")
         pbSubmarine = bSubmarine
-        print("p2 submarine hit")
+        print("BÄMM")
 
     elif bArray[positiony-1][positionx] == 4:
         bArray[positiony-1][positionx] = 1
@@ -190,7 +206,7 @@ while True:
         if (bCruiser < pbCruiser) and (bCruiser == 0):
             print("Player 2's Cruiser sank. ")
         pbCruiser = bCruiser
-        print("p2 cruiser hit")      
+        print("BÄMM")      
 
     elif bArray[positiony-1][positionx] == 5:
         bArray[positiony-1][positionx] = 1
@@ -199,8 +215,7 @@ while True:
         if (bMothership < pbMothership) and (bMothership == 0):
             print("Player 2's Mothership sank. ")
         pbMothership = bMothership 
-        print("p2 mothership hit") 
-
+        print("BÄMM") 
 
     elif bArray[positiony-1][positionx] == 6:  
         bArray[positiony-1][positionx] = 1
@@ -209,20 +224,17 @@ while True:
         if (bBattleship < pbBattleship) and (bBattleship == 0):
             print("Player 2's Battleship sank. ")
         pbBattleship = bBattleship 
-        print("p2 battleship hit")
-
-    nyomtat("a")
-
-
+        print("BÄMM")
+    playerSwitch()
 
     nyomtat("b")
+    print("      ↑↑↑↑↑↑↑ Your table ↑↑↑↑↑↑↑\n      ↓↓↓↓↓↓↓ Enemy table↓↓↓↓↓↓↓")
     nyomtat("bOcean")
     print("\n\nPlayer 2: Where would you like to shoot?")
     positionx = textToInt()
     positiony = int(input("Give the Y coordinate! (1-10) "))
     if aArray[positiony-1][positionx] == 0: 
         aArray[positiony-1][positionx] = 2
-        nyomtat("b")
         nyomtat("bOcean")
         print("miss")
         
@@ -233,7 +245,7 @@ while True:
         if (aSubmarine < paSubmarine) and (aSubmarine == 0):
             print("Player 1's Submarine sank. ")
         paSubmarine = aSubmarine
-        print("p1 submarine hit")
+        print("BÄMM")
 
     elif aArray[positiony-1][positionx] == 4:
         aArray[positiony-1][positionx] = 1
@@ -242,7 +254,7 @@ while True:
         if (aCruiser < paCruiser) and (aCruiser == 0):
             print("Player 1's Cruiser sank. ")
         paCruiser = aCruiser 
-        print("p1 cruiser hit")     
+        print("BÄMM")     
 
     elif aArray[positiony-1][positionx] == 5:
         aArray[positiony-1][positionx] = 1
@@ -251,8 +263,7 @@ while True:
         if (aMothership < paMothership) and (aMothership == 0):
             print("Player 1's Mothership sank. ")
         paMothership = aMothership  
-        print("p1 mothership hit")
-
+        print("BÄMM")
 
     elif aArray[positiony-1][positionx] == 6:  
         aArray[positiony-1][positionx] = 1
@@ -261,5 +272,5 @@ while True:
         if (aBattleship < paBattleship) and (aBattleship == 0):
             print("Player 1's Battleship sank. ")
         paBattleship = aBattleship 
-        print("p1 battleship hit")
-    nyomtat("b")
+        print("BÄMM")
+    playerSwitch()
